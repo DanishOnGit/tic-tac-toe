c7fc6d4bb7c8a89edd05f276331434a03dab9c38
DanishOnGit <youcandoit1911@gmail.com>
2021-08-04 04:31:15 +0530

    style:X sign added

---
+++ src/components/X.jsx
@@ -0,0 +1,11 @@
+
+
+export const X=()=>{
+    return (
+        <div className="x-sign-wrapper">
+           
+            <div className="inner-div rot-45"></div>
+            <div className="inner-div rot--45"></div>
+        </div>
+    )
+}
